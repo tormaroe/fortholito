fefb607cc5908823881203f3a11a9bce464a99e0
Torbjørn Marø <torbjorn.maro@gmail.com>
2013-01-07 17:32:11 +0100

    Using symbols for strings in defword

--- fortholito/vocabulary.rb
+++ fortholito/vocabulary.rb
@@ -16,54 +16,55 @@ module Fortholito
   module Vocabulary
     def defword name, action, options = {}
       @dictionary ||= {}
+      name = name.to_s if name.class == ::Symbol
       puts "Warning: Existing definition of '#{name}' changed" if @dictionary[name]
       @dictionary[name] = Word.new name, action, options
     end 
 
     def call_word word
-      action = @dictionary[word]
+      action = @dictionary[(if word.class == ::Symbol then word.to_s else word end)]  
       raise "Word '#{word}' undefined!" unless action
       action.call
     end
 
     def initialize_vocabulary
 
-      defword '+',    lambda { push pop + pop }
-      defword '-',    lambda { push pop - pop }
-      defword '*',    lambda { push pop * pop }
-      defword '/',    lambda { push pop / pop }
-      defword 'mod',  lambda { push pop % pop }
+      defword :+,    lambda { push pop + pop }
+      defword :-,    lambda { push pop - pop }
+      defword :*,    lambda { push pop * pop }
+      defword :/,    lambda { push pop / pop }
+      defword :mod,  lambda { push pop % pop }
 
       defword '=', lambda { push bool2flag pop == pop }
-      defword '<', lambda { push bool2flag pop > pop  }
-      defword '>', lambda { push bool2flag pop < pop  }
+      defword :<, lambda { push bool2flag pop > pop  }
+      defword :>, lambda { push bool2flag pop < pop  }
 
-      defword 'or', lambda {
+      defword :or, lambda {
         a, b = pop, pop
         push bool2flag (a == Fortholito::TRUE_FLAG or b == Fortholito::TRUE_FLAG) 
       }
-      defword 'and', lambda {
+      defword :and, lambda {
         a, b = pop, pop
         push bool2flag (a == Fortholito::TRUE_FLAG and b == Fortholito::TRUE_FLAG) 
       }
 
-      defword 'depth',  lambda { push @stack.size }
-      defword 'clear',  lambda { @stack = [] }
-      defword 'drop',   lambda { pop }
-      defword 'dup',    lambda { push @stack.last }
-      defword 'swap',   lambda {
+      defword :depth,  lambda { push @stack.size }
+      defword :clear,  lambda { @stack = [] }
+      defword :drop,   lambda { pop }
+      defword :dup,    lambda { push @stack.last }
+      defword :swap,   lambda {
         b, a = pop, pop
         push b
         push a
       }
-      defword 'over',   lambda {
+      defword :over,   lambda {
         b, a = pop, pop
         push a
         push b
         push a
       }, :doc => "( a b -- a b a )"
 
-      defword 'rot',   lambda {
+      defword :rot,   lambda {
         c, b, a = pop, pop, pop
         push b
         push c
@@ -71,28 +72,28 @@ module Fortholito
       }, :doc => "( a b c -- b c a )"
 
       defword '.',     lambda { print pop }, :doc => "( x --  ) Pop and print top value"
-      defword 'cr',    lambda { puts },      :doc => "Prints a newline"
-      defword 'space', lambda { print " " }, :doc => "Prints a space character"
+      defword :cr,    lambda { puts },      :doc => "Prints a newline"
+      defword :space, lambda { print " " }, :doc => "Prints a space character"
 
-      defword 'rand', lambda { push rand pop }, :doc => "( n1 -- n2 )"
+      defword :rand, lambda { push rand pop }, :doc => "( n1 -- n2 )"
       
       defword '.s'       , lambda { p @stack }, :doc => "Display stack once"
       
-      defword 'showstack', 
+      defword :showstack, 
         lambda { Fortholito.showstack = not(Fortholito.showstack) },
         :doc => "Use to toggle stack display on and off. Stack is displayed after each operation."
 
-      defword 'stacktrace', 
+      defword :stacktrace, 
         lambda { Fortholito.stacktrace = not(Fortholito.stacktrace) },
         :doc => "Use to toggle verbose stack display on and off. Stack is displayed each time it is modified. This is usually more information than you need :)"
 
-      defword 'words', lambda {
+      defword :words, lambda {
         @dictionary.keys.sort.each_slice(4) do |w|
           puts " " + w.map{|w| w.ljust 18}.join(" ")
         end
       }, :doc => "List all words in the vocabulary"
 
-      defword 'describe', lambda {
+      defword :describe, lambda {
         name = pop
         if name
           word = @dictionary[name] 
@@ -106,7 +107,7 @@ module Fortholito
         end
       }
 
-      defword 'help', lambda {
+      defword :help, lambda {
         [ " SOME USEFULL WORDS YOU SHOULD TRY:",
           "  words       \\ display all words in the vocabulary",
           "  describe    \\ pop word (string) and show details",
@@ -136,7 +137,7 @@ module Fortholito
           each {|line| puts line }
       }
 
-      defword 'bye', lambda { exit 0 }, :doc => "Quit REPL"
+      defword :bye, lambda { exit 0 }, :doc => "Quit REPL"
 
     end
 
